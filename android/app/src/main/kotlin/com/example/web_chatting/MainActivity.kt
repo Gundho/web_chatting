0cd870e19fc267ab132a97421a9e8b91c6d942e4
Gundho <55069284+Gundho@users.noreply.github.com>
2021-01-13 20:43:51 +0700

    upp

---
+++ android/app/src/main/kotlin/com/example/web_chatting/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.web_chatting
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+InAppLocalhostServer localhostServer = new InAppLocalhostServer();
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await localhostServer.start();
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hi! There!!',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  InAppWebViewController webView;
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        if (await webView.canGoBack()) {
+          webView.goBack();
+          return false;
+        }
+        return true;
+      },
+      child: Scaffold(
+        body: Container(
+            child: Column(children: <Widget>[
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.only(top: 30.0),
+              child: InAppWebView(
+                initialUrl: "http://localhost:8080/assets/login.html",
+                onWebViewCreated: (InAppWebViewController controller) {
+                  webView = controller;
+                },
+              ),
+            ),
+          )
+        ])),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    localhostServer.close();
+    super.dispose();
+  }
+}
